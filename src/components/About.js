0093b8f5bd4c4076ae5dafd9f1dd1fc08e9f45f4
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-05-21 10:57:34 +0300

    Completed the props lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,27 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+function About(props) {
+  console.log(props)
+  const bioText = props.bio
+
+  function bio(){
+    if(bioText) {
+    return <p>{props.bio}</p>
+    }
+  }
+
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      
+      {bio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}></Links>
     </div>
   );
 }
-
 export default About;
